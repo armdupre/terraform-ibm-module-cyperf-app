161309a63b9a0fb24d4350e6d88bd20c65ae1e0e
armdupre <a-joseph.dupre@keysight.com>
2023-06-05 08:24:59 -0400

    Initial Checkin.

---
+++ locals.tf
@@ -0,0 +1,20 @@
+locals {
+	Eth0FloatingIpName = "${local.Preamble}-floating-ip"
+	Eth0PrivateIpAddress = var.Eth0PrivateIpAddress	
+	Eth0SecurityGroupName = var.Eth0SecurityGroupName
+	Eth0SubnetName = var.Eth0SubnetName
+	ImageName = var.ImageName
+	InstanceId = var.InstanceId
+	InstanceName = "${local.Preamble}-instance"
+	InstanceProfile = var.InstanceProfile
+	Preamble = "${local.UserLoginTag}-${local.Tag}-${local.Version}-${local.InstanceId}"
+	SleepDelay = "7m"
+	SshKeyName = var.SshKeyName
+	Tag = var.Tag
+	UserEmailTag = var.UserEmailTag
+	UserLoginTag = var.UserLoginTag
+	UserProjectTag = var.UserProjectTag
+	Version = var.Version
+	VpcName = var.VpcName
+	Zone = var.Zone
+}
